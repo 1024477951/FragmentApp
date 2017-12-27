5d7bf52b6f74b3f4cca48ab474a21be57c4dd2c1
liuzhen32 <1024477951@qq.com>
2017-12-27 17:25:47 +0800

    LeakCanary库添加

--- app/src/main/java/com/fragmentapp/App.java
+++ app/src/main/java/com/fragmentapp/App.java
@@ -2,6 +2,8 @@ package com.fragmentapp;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
+
 /**
  * Created by liuzhen on 2017/11/8.
  */
@@ -16,6 +18,12 @@ public class App extends Application {
         instance = this;
         //第一：默认初始化
 //        Bmob.initialize(this, "7a0cb418fc0379a4b199c6b1a51e5ca9");
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
     }
 
     public static App getInstance(){
