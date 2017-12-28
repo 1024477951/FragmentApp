037ddc423dede98d33fac83618f35bf2570ab6b8
liuzhen32 <1024477951@qq.com>
2017-12-28 14:52:04 +0800

    lazy

--- app/src/main/java/com/fragmentapp/base/BaseFragment.java
+++ app/src/main/java/com/fragmentapp/base/BaseFragment.java
@@ -43,7 +43,6 @@ public abstract class BaseFragment extends Fragment{
             }
         }
         isPrepared = true;
-        init();
         return contentView;
     }
 
--- app/src/main/java/com/fragmentapp/base/LazyFragment.java
+++ app/src/main/java/com/fragmentapp/base/LazyFragment.java
@@ -1,6 +1,10 @@
 package com.fragmentapp.base;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.util.Log;
+
+import com.fragmentapp.helper.EmptyLayout;
 
 /**
  * Created by liuzhen on 2017/11/10.
@@ -8,7 +12,15 @@ import android.util.Log;
 
 public abstract class LazyFragment extends BaseFragment {
 
-    protected boolean isLoad;//是否加载完
+    protected boolean isLoad = false;//是否加载完
+
+    protected EmptyLayout emptyLayout;
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        onVisible();
+    }
 
     /**
      * 在这里实现Fragment数据的缓加载.
@@ -23,13 +35,21 @@ public abstract class LazyFragment extends BaseFragment {
             isVisible = false;
         }
     }
-
+    /**可见时调用*/
     protected void onVisible(){
-        if(!isPrepared || !isVisible || !isLoad) {
+        if(!isPrepared || !isVisible || isLoad) {
             return;
         }
         init();
-        Log.e("tag","init success");
+        isLoad = true;//加载完后更改状态，只限定加载一次
+        emptyLayout = new EmptyLayout(getActivity());
+//        Log.e("tag","onVisible");
     }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        emptyLayout.clear();
+        emptyLayout = null;
+    }
 }
