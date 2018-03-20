b33f2d018ed4b0f237df41eaeaf5d9f74fc6c35f
liuzhen32 <1024477951@qq.com>
2018-03-20 14:04:12 +0800

    util

---
+++ app/src/main/java/com/fragmentapp/helper/MBUtil.java
@@ -0,0 +1,13 @@
+package com.fragmentapp.helper;
+
+/**
+ * Created by liuzhen on 2018/3/20.
+ */
+
+public class MBUtil {
+
+    public static String formatSize(long size) {
+        return size < 1024L?String.format("%d B", new Object[]{Integer.valueOf((int)size)}):(size < 1048576L?String.format("%.2f KB", new Object[]{Float.valueOf((float)size / 1024.0F)}):(size < 1073741824L?String.format("%.2f MB", new Object[]{Float.valueOf((float)size / 1048576.0F)}):String.format("%.2f G", new Object[]{Float.valueOf((float)size / 1.07374182E9F)})));
+    }
+
+}
