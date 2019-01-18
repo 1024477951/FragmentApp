e6705d5b6394763ed1078c43600c3d3f3cec8105
liuzhen32 <1024477951@qq.com>
2019-01-18 18:37:17 +0800

    keyboard

--- app/src/main/java/com/fragmentapp/dynamic/adapter/KeyboardImgAdapter.java
+++ app/src/main/java/com/fragmentapp/dynamic/adapter/KeyboardImgAdapter.java
@@ -1,7 +1,10 @@
 package com.fragmentapp.dynamic.adapter;
 
+import android.view.View;
+
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
+import com.fragmentapp.R;
 
 /**
  * Created by liuzhen on 2018/11/28.
@@ -16,7 +19,12 @@ public class KeyboardImgAdapter extends BaseQuickAdapter<String,BaseViewHolder>{
     }
 
     @Override
-    protected void convert(BaseViewHolder helper, String item) {
-
+    protected void convert(final BaseViewHolder helper, String item) {
+        helper.getView(R.id.iv_del).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                remove(helper.getAdapterPosition());
+            }
+        });
     }
 }
